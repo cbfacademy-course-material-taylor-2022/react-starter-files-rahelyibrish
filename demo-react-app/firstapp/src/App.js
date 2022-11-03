a771127bca63be41d980cd7feccdc3806690eaa2
rahelyibrish <95376576+rahelyibrish@users.noreply.github.com>
2022-11-03 23:14:54 +0300

    updated css file

---
+++ demo-react-app/firstapp/src/App.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rahel's React Page</h1>
+      <h2 className="bio">
+        This is my first React page which has been fun to build.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
