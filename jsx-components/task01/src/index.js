86282d9b26ad49dcda2af2c2c92e14f4708b5682
rahelyibrish <95376576+rahelyibrish@users.noreply.github.com>
2022-11-08 23:48:03 +0300

    updated react file

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const name = '[YOUR NAME GOES HERE]';
+const name = "Rahel";
 
-function Profile(name){
-    return <main id="content" role="main" className="base">
-            <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
-            </main>
+function Profile(name) {
+  if (name.length > 0) {
+    return (
+      <main id="content" role="main" className="base">
+        <h1>{name}'s React Page</h1>
+        <p>Example of react JSX in action</p>
+      </main>
+    );
+  } else {
+    return (
+      <main id="content" role="main" className="base">
+        <h1>Just a React Page</h1>
+        <p>Example of react JSX in action</p>
+      </main>
+    );
+  }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Profile(name));
