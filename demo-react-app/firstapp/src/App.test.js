2e052a2d26f02e024b41bda4b5f283bd5a173ec0
rahelyibrish <95376576+rahelyibrish@users.noreply.github.com>
2022-11-08 13:54:10 +0300

    react part 2

--- demo-react-app/firstapp/src/App.test.js
+++ demo-react-app/firstapp/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-it("renders learn react link", () => {
+it("renders teach react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Rahel/i);
   expect(linkElement).toBeInTheDocument();
 });
