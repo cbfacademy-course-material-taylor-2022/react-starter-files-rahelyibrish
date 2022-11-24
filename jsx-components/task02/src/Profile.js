26f918df2642b04c06065022d75ecd45ab35257d
rahelyibrish <95376576+rahelyibrish@users.noreply.github.com>
2022-11-24 15:53:38 +0000

    update to profiles

--- jsx-components/task02/src/Profile.js
+++ jsx-components/task02/src/Profile.js
@@ -1,5 +1,27 @@
+function Profile(props) {
+  if (props.name && props.bio) {
+    return (
+      <main id="content" role="main" className="base">
+        <h1>{props.name}'s React Page</h1>
+        <p>{props.bio}</p>
+        <h2>{props.color}</h2>
+      </main>
+    );
+  } else if (props.name !== "") {
+    return (
+      <main id="content" role="main" className="base">
+        <h1>Nobody's React Page</h1>
+        <p>Nobody lives here.</p>
+      </main>
+    );
+  } else {
+    return (
+      <main id="content" role="main" className="base">
+        <h1>Just a react page.</h1>
+        <p>Nothing to see here.</p>
+      </main>
+    );
+  }
+}
 
-/* Add the Profile function here */
-
-
-export default Profile
+export default Profile;
--- jsx-components/task02/src/index.js
+++ jsx-components/task02/src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Profile from './Profile';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Profile from "./Profile";
 
 function App() {
   return (
-      //Replace the string below with the JSX Profile component
-      "Return the JSX Profile function as a JSX element here"
+    //Replace the string below with the JSX Profile component
+    <Profile name="Eva" bio="likes travelling." />
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
