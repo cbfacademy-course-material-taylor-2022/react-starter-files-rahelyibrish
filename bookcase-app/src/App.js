6713c33ac80ce3955798f1e79b1ca10b3fc49e89
rahelyibrish <95376576+rahelyibrish@users.noreply.github.com>
2022-11-25 12:57:49 +0000

    update to book.js

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import Book from './components/Book';
-import data from './models/books.json';
+import React, { useState } from "react";
+import Book from "./components/Book2";
+import Search from "./components/Search";
+import data from "./models/books.json";
 
 function App() {
-  const books = data;
-  return <Book/>;
+  const [books, setBooks] = useState(data);
+
+  async function findBooks(value) {
+    console.log("search value:", value);
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${value}&filter=paid-ebooks&print-type=books&projection=lite`;
+    const results = await fetch(url).then((res) => res.json());
+    if (!results.error) {
+      setBooks(results.items);
+    }
+  }
+
+  return (
+    <>
+      <Search findBooks={findBooks} />
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </>
+  );
 }
 
 export default App;
-
--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function Book() {
   return "This is the output of a book component. Replace this string with code to display an individual book's attributes.";
